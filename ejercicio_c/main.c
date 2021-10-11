232c815507cf2b2c1910f2465725a6d28a8fc9da
pato12escalante <89897463+pato12escalante@users.noreply.github.com>
2021-10-11 08:02:15 -0500

    Add files via upload

---
+++ ejercicio_c/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include "configbits.h"
+
+#define _XTAL_FREQ 4000000
+
+char frec, ciclodet, presc;
+
+int frecuencia, tiempo_on;
+
+void pwmON(frec, ciclodet){
+    T2CONbits.TMR2ON = 1;
+
+    presc=16;
+    //PARA FRECUENCIA
+    frecuencia=(1000000)/((frec*presc))-1;
+    if (frecuencia < 256){
+        PR2 = frecuencia;
+    }
+    else{
+        presc=4;
+        frecuencia=(1000000)/((frec*presc))-1;
+        if (frecuencia < 256){
+            PR2 = frecuencia;
+        }
+        else{
+            presc=1;
+            frecuencia=(1000000)/((frec*presc))-1;
+            if (frecuencia < 256){
+                PR2 = frecuencia;
+            }
+        }
+    }
+
+    
+    //PARA DUTY CICLE (FRECUENCIA)
+    tiempo_on=(40000*ciclodet)/(frec*presc);
+    CCPR1L = tiempo_on>>2;
+    CCP1CONbits.DC1B = tiempo_on & 0b00000011;
+    
+    return;
+    
+}
+
+void pwmOFF(){
+    T2CONbits.TMR2ON = 0;
+}
+
+void PWMinit(){
+    
+    //CONFIGURAR EL MODO PWM en el pin RC2
+    CCP1CON = 0b00001100;
+    //ESTABLECER LA FRECUENCIA DE OPERACIÓN (UTILIZANDO LA FÓRMULA)
+    
+    PR2 = frecuencia;
+    //ESTABLECER EL PREESCALER, EN ESTE CASO 1:16
+
+    T2CONbits.T2CKPS = 0b11;
+
+    //ACTIVAR EL TIMER2
+    T2CONbits.TMR2ON = 1;
+
+    //ESTABLECER EL CICLO DE TRABAJO,10 BITS EN TOTAL
+    CCPR1L = tiempo_on>>2;
+    CCP1CONbits.DC1B = tiempo_on & 0b00000011;
+  
+    //CONFIGURAR PUERTO C COMO SALIDA (SÓLO SE UTILIZARÁ RC2)
+    TRISC = 0;
+    
+}
+
+
+void main() {
+    
+    PWMinit();
+    
+    
+    while(1){
+        for(int i=10;i<=90;i+=10){
+            pwmON(1000, i);
+            __delay_ms(100);
+        }
+        
+        for(int i=90;i>=10;i-=10){
+            pwmON(1000, i);
+            __delay_ms(100);
+        }
+        
+ 
+    }
+    
+
+}
